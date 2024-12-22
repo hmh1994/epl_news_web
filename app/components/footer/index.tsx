2b348df85e27f5646bb12ca7f866b8419f3bf94e
june244 <kyjune9993@gmail.com>
2024-12-22 16:46:07 +0900

    simple styling

--- app/components/footer/index.tsx
+++ app/components/footer/index.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className='mx-auto flex gap-6 flex-wrap items-center justify-center bg-zinc-800 h-[10vh]'>
+    <footer className='mx-auto flex gap-6 flex-wrap items-center justify-center bg-epl-main h-[10vh]'>
       FOOTER
     </footer>
   );
