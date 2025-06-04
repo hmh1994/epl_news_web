30ea847efc43710139b108ba29e2e322a2b1e600
june244 <kyjune9993@gmail.com>
2025-06-04 21:39:48 +0900

    seo

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,21 +2,42 @@ import type { Metadata } from "next";
 import "../globals.css";
 import { Footer, Header } from "@/components/common";
 
-export const metadata: Metadata = {
-  title: "Infootball - 최신 축구 매치·뉴스·선수 정보",
-  description:
-    "인풋풋볼에서 국내외 축구 매치 일정, 선수 정보, 팀 소식 등을 빠르게 확인하세요.",
-  openGraph: {
-    title: "인풋풋볼",
-    description: "축구 매치·뉴스·선수 정보 플랫폼",
-    url: "https://infootball.kr",
-    siteName: "인풋풋볼",
-  },
-  robots: {
-    index: true,
-    follow: true,
-  },
-};
+// export const metadata: Metadata = {
+//   title: "Infootball - 최신 축구 매치·뉴스·선수 정보",
+//   description:
+//     "인풋풋볼에서 국내외 축구 매치 일정, 선수 정보, 팀 소식 등을 빠르게 확인하세요.",
+//   openGraph: {
+//     title: "인풋풋볼",
+//     description: "축구 매치·뉴스·선수 정보 플랫폼",
+//     url: "https://infootball.kr",
+//     siteName: "인풋풋볼",
+//   },
+//   robots: {
+//     index: true,
+//     follow: true,
+//   },
+// };
+
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: "인풋볼 - 최신 축구 매치·뉴스·선수 정보",
+    description:
+      "인풋볼에서 국내외 축구 매치 일정, 선수 정보, 팀 소식 등을 빠르게 확인하세요.",
+    openGraph: {
+      title: "인풋볼",
+      description: "축구 매치·뉴스·선수 정보 플랫폼",
+      url: "https://infootball.kr",
+      siteName: "인풋풋볼",
+    },
+    robots: {
+      index: true,
+      follow: true,
+      googleBot: {
+        index: true,
+      },
+    },
+  };
+}
 
 export default async function RootLayout({
   children,
